09bcc518351eb1736e8ec9b0cb3a0e2967d322df
Yogesh Kumar <yogeshkumar180592@gmail.com>
2017-04-15 23:42:14 +0530

    Added the initial frontend, starting with the redux store and react router

---
+++ app/src/containers/Root.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Root extends Component {
+  return {
+    render(
+      <h1>Welcome to the root directory</h1>
+    )
+  }
+}
--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -6,6 +6,19 @@ const Wallet = require('../models/Wallet')
 
 const sendtransaction = (req, res, next) => {
 
-  selectedWallet = walletUtil.getSelectedWallet(req.wallets, req.params.walletId)
-  
+  selectedWallet = walletUtil.getSelectedWallet(req.wallets, req.body.walletId)
+
+  let options = {
+    amount: req.body.amount,
+    address: req.body.address,
+    message: req.body.message || null
+  }
+
+  walletUtil.createSendTransaction(selectedWallet.client, options)
+    .then((memo) => {
+      return res.status(200).send(memo)
+    })
+    .catch((err) => {
+      return next(err)
+    })
 }
