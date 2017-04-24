e2d105f3cdee0f23a5d2c975fb5c9b1f319d2601
Yogesh Kumar <yogeshkumar180592@gmail.com>
2017-04-25 02:02:34 +0530

    Updated the install script. Readme now includes task list for both frontend and backend

--- install.sh
+++ install.sh
@@ -3,9 +3,13 @@
 GREEN='\033[0;32m'
 NOCOLOR='\033[0m'
 
+echo -e "${GREEN}Installing global dependencies ${NOCOLOR}"
+npm install -g eslint yarn
+
 echo -e "${GREEN}Installing frontend dependencies ${NOCOLOR}"
 cd app && yarn install
 
+echo -e "${GREEN}Going back to parent directory ${NOCOLOR}"
 cd ..
 
 echo -e "\n${GREEN}Installing backend dependencies ${NOCOLOR}"
